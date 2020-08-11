6624b82c37b24683aa915fb812b44374bd4f58cf
powernode <powernode@126.com>
2020-08-11 10:30:24 +0800

    创建学生类

---
+++ src/main/java/com/powernode/git/Student.java
@@ -0,0 +1,17 @@
+package com.powernode.git;
+
+public class Student {
+
+    private String id;
+
+    private String name;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
